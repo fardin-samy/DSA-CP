9d4259604dc4d5b4804822fe5b3951b20ea2176e
Fardin Hasan Samy <137752176+fardin-samy@users.noreply.github.com>
2024-08-12 20:20:01 +0600

    Create primesUnderN.cpp

---
+++ primesUnderN.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e7 + 9;
+int d[N];
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  for (int i = 1; i < N; i++) {
+    for (int j = i; j < N; j += i) {
+      d[j]++;
+    }
+  }
+  vector<int> primes;
+  for (int i = 1; i < N; i++) {
+    if (d[i] == 2) {
+      primes.push_back(i);
+    }
+  }
+  cout << primes.size() << '\n';
+  return 0;
+}
