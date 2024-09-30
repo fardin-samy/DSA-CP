777e3601d133637d63b6677f59706052cf4d10f0
Fardin Hasan Samy <137752176+fardin-samy@users.noreply.github.com>
2024-09-30 17:19:43 +0600

    Update pbds.cpp

--- ordered_set/pbds.cpp
+++ ordered_set/pbds.cpp
@@ -5,3 +5,6 @@
 #include <ext/pb_ds/tree_policy.hpp> 
 using namespace __gnu_pbds; 
 template<class T> using ordered_set = tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>;
+
+//order_of_key (k) : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).
